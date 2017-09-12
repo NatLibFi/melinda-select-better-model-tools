fbc089ec4e42a29340d984569b4f01533bdfdce2
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-12 17:52:45 +0300

    ref upd, labels to plot

--- plot.r
+++ plot.r
@@ -1,9 +1,9 @@
 data <- read.table('table.txt')
 
-png(file="training-plot.png", bg="white")
+png(file="error.png", bg="white", width=1200, height=600)
 
 
-plot(data, type="n")
+plot(data, type="n", xlab="Iterations", ylab="Error")
 lines(data)
 
 dev.off()
